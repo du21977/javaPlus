cb922dc3c460e6fd9ac2d5e7fa6f271cc413f2a8
dobi <549914283@qq.com>
2018-11-15 10:58:04 +0800

    线程并发与线程池

--- ThreadDemo/src/main/java/com/dobi/day5/NewCachedThreadTest.java
+++ ThreadDemo/src/main/java/com/dobi/day5/NewCachedThreadTest.java
@@ -7,7 +7,8 @@ import java.util.concurrent.Executors;
 public class NewCachedThreadTest {
 
 	public static void main(String[] args) {
-		// 创建可缓存线程池
+		// 创建可缓存线程池----线程用完不会回收，会放到线程池
+		//程序执行完，程序不会退出，程序还在，因为线程池没有销毁，要等一段时间，线程池才销毁，程序才退出，可以看红点
 		ExecutorService newCachedThreadPool = Executors.newCachedThreadPool();
 		// 执行execute 表示创建了线程。 类似 start
 		for (int i = 0; i < 30; i++) {
